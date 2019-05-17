a6bc54cefff4307fda3896ceddec362f72a2e7bc
motty <admin@fujicoin.org>
2019-05-17 21:01:08 +0900

    mod mother seed

--- main.cpp
+++ main.cpp
@@ -397,7 +397,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"seed1.fujicoin.org", ""};
+static const string mainnet_seeds[] = {"seed.fujicoin.org", ""};
 static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
